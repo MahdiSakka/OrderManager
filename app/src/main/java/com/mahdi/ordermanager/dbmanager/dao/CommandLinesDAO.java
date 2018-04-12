81d52298a18178e8a6637c83a398c1dff3ecb66c
MahdiSakka <38231057+MahdiSakka@users.noreply.github.com>
2018-04-12 23:28:23 +0100

    Add CommandLinesDAO class

---
+++ app/src/main/java/com/mahdi/ordermanager/dbmanager/dao/CommandLinesDAO.java
@@ -0,0 +1,43 @@
+package com.mahdi.ordermanager.dbmanager.dao;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import com.mahdi.ordermanager.dbmanager.model.Command;
+import com.mahdi.ordermanager.dbmanager.model.Product;
+
+public class CommandLinesDAO {
+    private SQLiteDatabase database;
+
+    public static final String TABLE_NAME = "COMMAND_LINES";
+    public static final String KEY = "id";
+    public static final String COMMAND_ID = "commandID";
+    public static final String PRODUCT_ID = "productID";
+    public static final String QUANTITY = "quantity";
+
+    public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "( " +
+                    KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COMMAND_ID + " INTEGER NOT NULL, " +
+                    PRODUCT_ID + " INTEGER NOT NULL, " +
+                    QUANTITY + " INTEGER, " +
+            "CONSTRAINT FK1 FOREIGN KEY " + COMMAND_ID + " REFERENCES " + CommandDAO.TABLE_NAME + "(" + CommandDAO.KEY + "), " +
+            "CONSTRAINT FK2 FOREIGN KEY " + PRODUCT_ID + " REFERENCES " + ProductDAO.TABLE_NAME + "(" + ProductDAO.KEY + ")," +
+            "PRIMARY KEY (" + COMMAND_ID + ", " + PRODUCT_ID + "));";
+
+    public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+
+    public CommandLinesDAO (SQLiteDatabase database) {
+        this.database = database;
+    }
+
+    public void createTable () {
+        this.database.execSQL(CREATE_TABLE);
+    }
+
+    public void removeTable() {
+        this.database.execSQL(DROP_TABLE);
+    }
+
+    public void addCommandLine (Product product) {
+
+    }
+}
---
+++ app/src/main/java/com/mahdi/ordermanager/dbmanager/model/CakeTaste.java
@@ -0,0 +1,8 @@
+package com.mahdi.ordermanager.dbmanager.model;
+
+public enum CakeTaste {
+    Hazelnuts,
+    Pistachio,
+    Mocha,
+    NutsCoffee,
+}
