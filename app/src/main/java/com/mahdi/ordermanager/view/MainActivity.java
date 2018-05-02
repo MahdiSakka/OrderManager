c010775bb8da50707e8de0bc4b02c3da3a1f837d
MahdiSakka <38231057+MahdiSakka@users.noreply.github.com>
2018-05-02 21:11:20 +0100

    Update code / add settings view

--- app/src/main/java/com/mahdi/ordermanager/view/MainActivity.java
+++ app/src/main/java/com/mahdi/ordermanager/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mahdi.ordermanager.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -70,6 +71,9 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, Settings.class);
+
+            startActivity(intent);
             return true;
         }
 
---
+++ app/src/main/java/com/mahdi/ordermanager/view/Settings.java
@@ -0,0 +1,26 @@
+package com.mahdi.ordermanager.view;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.mahdi.ordermanager.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+}
